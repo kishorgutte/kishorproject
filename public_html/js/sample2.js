43cdde67e94f349639f3d99bcb409c15637a10c8
Kishor Gutte <kishor.gutte@agreeyamobility.net>
2015-11-07 19:52:01 +0530

    Adding feature

--- public_html/js/sample2.js
+++ public_html/js/sample2.js
@@ -4,3 +4,23 @@
  * and open the template in the editor.
  */
 
+ $(function() {
+                $('.kc-wrap').KillerCarousel({
+                    // Default natural width of carousel.
+                    width: 800,
+                    // Item spacing in 3d (has CSS3 3d) mode.
+                    spacing3d: 120,
+                    // Item spacing in 2d (no CSS3 3d) mode. 
+                    spacing2d: 120,
+                    showShadow: false,
+                    showReflection: false,
+                    // Looping mode.
+                    infiniteLoop: true,
+                    // Scale at 75% of parent element.
+                    autoScale: 75,
+                });
+            });
+            
+           
+            
+            $('.kc-horizon').css({" perspective": "0px"})
