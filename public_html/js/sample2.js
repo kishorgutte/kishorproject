ea0a0f57f015e0565bae066554f133a73a047be9
kishor.gutte <kishor.gutte@agreeyamobility.net>
2015-11-19 00:30:11 +0530

    Fixed issue of cors

--- public_html/js/sample2.js
+++ public_html/js/sample2.js
@@ -3,6 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+
  $(function() {
                 $('.kc-wrap').KillerCarousel({
                     // Default natural width of carousel.
